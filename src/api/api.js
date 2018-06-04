97962c9e6082978a42c3c87f6413f94cd3f8f0e5
maoyaY <1335679005@qq.com>
2018-06-04 21:50:18 +0800

    接口地址

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,17 @@ import {
     wxRequest
 } from '../utils/wxRequest';
 
-const api2 = 'https://min3.pinecc.cn/';
+const api2 = 'https://mini3.pinecc.cn/';
 
 //登录 注册
 
 const login = (params) => wxRequest(params, api2+'/goods/users/login')
-const register = (params) => wxRequest(params, api2+'/goods/users/register')
-
+const register = (params) => wxRequest(params, api2+'/goods/users/register');
+const disttrictList = (params) => wxRequest(params,api2 + 'api/Addr/disttrictList.html');
+const blList = (params) => wxRequest(params,api2 + 'api/User/blList.html');
 
 //评价
 module.exports = {
-    register, login
+    register, login,disttrictList,blList
 
 }
