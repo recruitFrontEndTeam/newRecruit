7c84276090161d27e61d061b82893c4d56637a77
maoyaY <1335679005@qq.com>
2018-06-05 20:56:19 +0800

    sess_key‘

--- src/api/api.js
+++ src/api/api.js
@@ -8,11 +8,13 @@ const api2 = 'https://mini3.pinecc.cn/';
 
 const login = (params) => wxRequest(params, api2+'/goods/users/login')
 const register = (params) => wxRequest(params, api2+'/goods/users/register');
+const workTypeList = (params) => wxRequest(params, api2 + 'api/Work/workTypeList.html');
+const workList = (params) => wxRequest(params, api2 + 'api/Work/workList.html');
 const disttrictList = (params) => wxRequest(params,api2 + 'api/Addr/disttrictList.html');
 const blList = (params) => wxRequest(params,api2 + 'api/User/blList.html');
 
 //评价
 module.exports = {
-    register, login,disttrictList,blList
+    register, login,workList,workTypeList,disttrictList,blList
 
 }
