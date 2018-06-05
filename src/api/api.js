048d59fcec440dae32858ed12bb84667987ba4c5
maoyaY <1335679005@qq.com>
2018-06-05 21:37:33 +0800

    伯乐排行，城市

--- src/api/api.js
+++ src/api/api.js
@@ -6,15 +6,13 @@ const api2 = 'https://mini3.pinecc.cn/';
 
 //登录 注册
 
-const login = (params) => wxRequest(params, api2+'/goods/users/login')
-const register = (params) => wxRequest(params, api2+'/goods/users/register');
-const workTypeList = (params) => wxRequest(params, api2 + 'api/Work/workTypeList.html');
+const login = (params) => wxRequest(params, api2+'api/Login/login.html');
 const workList = (params) => wxRequest(params, api2 + 'api/Work/workList.html');
+const workTypeList = (params) => wxRequest(params, api2 + 'api/Work/workTypeList.html');
 const disttrictList = (params) => wxRequest(params,api2 + 'api/Addr/disttrictList.html');
 const blList = (params) => wxRequest(params,api2 + 'api/User/blList.html');
-
 //评价
 module.exports = {
-    register, login,workList,workTypeList,disttrictList,blList
+  login,workList,workTypeList,disttrictList,blList
 
 }
