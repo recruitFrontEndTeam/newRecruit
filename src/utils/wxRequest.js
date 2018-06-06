ce17322144fd85416e06bb29332b214f2ce73c88
maoyaY <1335679005@qq.com>
2018-06-06 20:18:27 +0800

    首页定位加滚动加载更多

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -4,7 +4,6 @@ import tip from './tip'
 const wxRequest = async(params = {}, url) => {
   tip.loading();
   let data = params.query || {};
-
   let res = await wepy.request({
     url: url,
     method: params.method || 'GET',
